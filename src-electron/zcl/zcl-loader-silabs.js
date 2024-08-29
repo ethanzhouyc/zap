de0d41bd961b83b1fe2c05e8e8e0dc61aeb79c4e
Ethan.Z <Ethan.Zhou@silabs.com>
2024-08-29 14:26:25 -0400

    fix FPG typo in xml

--- src-electron/zcl/zcl-loader-silabs.js
+++ src-electron/zcl/zcl-loader-silabs.js
@@ -2274,7 +2274,7 @@ async function parseFeatureFlags(db, packageId, featureFlags) {
  *                 { "feature": [
  *                      { "$": {"name": "PIN"}},
  *                      { "$": {"name": "RID"}},
- *                      { "$": {"name": "FPG"}},
+ *                      { "$": {"name": "FGP"}},
  *                      { "$": {"name": "FACE"}}
  *                   ]
  *                 }
@@ -2286,7 +2286,7 @@ async function parseFeatureFlags(db, packageId, featureFlags) {
  * }
  *
  * Output conformance string:
- *  "Matter & (PIN | RID | FPG | FACE)"
+ *  "Matter & (PIN | RID | FGP | FACE)"
  *
  * The baseLevelTerms variable include terms that can not have nested terms.
  * When they appear, stop recursing and return the name inside directly
